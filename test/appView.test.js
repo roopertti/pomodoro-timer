5afc5373a57a6a6adc0a226bc76e0b771b2f2858
Robert Kuhlmann <robert.kuhlmann2@gmail.com>
2021-06-10 00:36:20 +0300

    test: moved App level test to test directory

--- test/appView.test.js
+++ test/appView.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 
-import App from './App'
+import App from '../src/App'
 
 describe('App component tests', () => {
   it('render App', () => {
